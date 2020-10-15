82f04ea5da621d78c50a8d4547f710ab6552c2a4
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-15 09:51:50 -0300

    Aula 3: Listando empresas com servlets

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,16 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	
+	//Artimanha para carregar essas duas empresas sempre que o servidor subir
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
 
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
