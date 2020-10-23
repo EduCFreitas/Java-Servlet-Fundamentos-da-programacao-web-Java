8f154abc62bdf6bffdf6fec093b6bef0ec82c6a6
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-23 15:01:05 -0300

    Aula 2.5: Filtro de autorização

--- gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -15,6 +15,7 @@ public class MonitoramentoFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		System.out.println("MonitoramentoFilter");
 
 		long antes = System.currentTimeMillis();
 		
